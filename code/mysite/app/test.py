c71b545b8565f901ed89d24a852090307c1ce356
jonnyboi02 <71353978+jonnyboi02@users.noreply.github.com>
2023-02-06 13:33:43 +0000

    running the bootnode

--- code/mysite/app/test.py
+++ code/mysite/app/test.py
@@ -1,8 +1,6 @@
 import os
+
 def createNode(password):
-    #os.chdir("../")
-    #os.mkdir("../")
-    #print(os.getcwd(), "!")
     os.chdir("gethpoa")
     i = 1
     dir = "node"
@@ -10,24 +8,34 @@ def createNode(password):
     while os.path.exists(check):
         i+=1
         check = dir+str(i)
-    print(check)
     os.mkdir(check)
     os.chdir(check)
     #Creates textfile containing password to the node
     with open("password.txt", 'w') as f:
         f.write(password)
-
     #Running geth on a new node
-    os.system("geth --datadir ./data account new --password password.txt")
+    #os.system("geth --datadir ./data account new --password password.txt")
+    output = os.popen("geth --datadir ./data account new --password password.txt").read()
+    arr = output.split("\n")
+    keyindex = arr[3].split(" ")
+    #Store the public key somewhere...
+    public_key = keyindex[len(keyindex)-1]
     #Creates genesis file on the node
     os.system("geth --datadir ./data init ../blockpoa.json")
-    #os.system("dir")
-    os.chdir("../../")
-    os.getcwd()
+    os.chdir("../")
+    os.chdir("bnode")
+    #Starts the bootnode within bnode folder
+    os.system('bootnode -nodekey "./boot.key" -verbosity 7 -addr "127.0.0.1:30301"')
+    #Need to use applescript to create new terminal and run command
+    
+    os.chdir("../")
+
 def create_node():
     res = input("Enter password")
     createNode(res)
-#    return res
+
+
+
 
 #createNode()
 create_node()
