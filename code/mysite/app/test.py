4b4985dce2228364e1943d7ddcabeba72dcd3ddd
jonnyboi02 <71353978+jonnyboi02@users.noreply.github.com>
2023-02-06 12:43:01 +0000

    creation of node on blockchain

--- code/mysite/app/test.py
+++ code/mysite/app/test.py
@@ -1,16 +1,33 @@
 import os
-def createNode():
+def createNode(password):
     #os.chdir("../")
     #os.mkdir("../")
-    print(os.getcwd(), "!")
+    #print(os.getcwd(), "!")
     os.chdir("gethpoa")
     i = 1
     dir = "node"
     check = dir+str(i)
-    while os.path.exists(""):
+    while os.path.exists(check):
+        i+=1
         check = dir+str(i)
-        
+    print(check)
+    os.mkdir(check)
+    os.chdir(check)
+    #Creates textfile containing password to the node
+    with open("password.txt", 'w') as f:
+        f.write(password)
 
-    print(os.getcwd(), "!")
+    #Running geth on a new node
+    os.system("geth --datadir ./data account new --password password.txt")
+    #Creates genesis file on the node
+    os.system("geth --datadir ./data init ../blockpoa.json")
+    #os.system("dir")
+    os.chdir("../../")
+    os.getcwd()
+def create_node():
+    res = input("Enter password")
+    createNode(res)
+#    return res
 
-createNode()
+#createNode()
+create_node()
